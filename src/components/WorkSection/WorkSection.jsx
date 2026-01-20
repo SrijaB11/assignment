88191443fdf15adfda4df5f28cf176db377d5eb1
SrijaB11 <boddusrija11@gmail.com>
2026-01-20 08:57:30 +0530

    fourth

--- src/components/WorkSection/WorkSection.jsx
+++ src/components/WorkSection/WorkSection.jsx
@@ -141,10 +141,150 @@
 //   );
 // }
 
+//
+//
+// import { Box, Typography } from "@mui/material";
+// import Grid from "@mui/material/Unstable_Grid2"; // ✅ FIX
+// import StepCard from "./StepsCard";
+// import women from "../../assets/images/Image.png";
+
+// import arrow1 from "../../assets/vectors/arrow1.png";
+// import arrow2 from "../../assets/vectors/arrow2.png";
+
+// const steps = [
+//   "Sign Up",
+//   "Code and Learn",
+//   "Understand",
+//   "Practice",
+//   "Achieve",
+// ];
+
+// export default function WorkSection() {
+//   return (
+//     <Box
+//       sx={{
+//         position: "relative",
+//         overflow: "hidden",
+//         minHeight: { xs: "auto", md: "860px" },
+//       }}
+//     >
+//       {/* BACKGROUND */}
+//       <Box
+//         sx={{
+//           position: "absolute",
+//           inset: 0,
+//           bgcolor: "primary.main",
+//           zIndex: 0,
+//         }}
+//       />
+
+//       {/* CONTENT */}
+//       <Box sx={{ position: "relative", zIndex: 1, px: { xs: 2, md: 6 } }}>
+//         <Grid container spacing={{ xs: 4, md: 6 }} alignItems="flex-start">
+//           {/* LEFT IMAGE */}
+//           <Grid xs={12} md={6}>
+//             <Box textAlign={{ xs: "center", md: "left" }}>
+//               <Box
+//                 component="img"
+//                 src={women}
+//                 alt="Student"
+//                 sx={{
+//                   width: "100%",
+//                   maxWidth: { xs: 320, sm: 420, md: 520 },
+//                   display: "block",
+//                   mx: { xs: "auto", md: 0 },
+//                 }}
+//               />
+//             </Box>
+//           </Grid>
+
+//           {/* RIGHT CONTENT */}
+//           <Grid xs={12} md={6} position="relative">
+//             <Typography
+//               variant="h3"
+//               color="white"
+//               mb={2}
+//               textAlign={{ xs: "center", md: "left" }}
+//               pt={{ xs: 0, md: 4 }}
+//             >
+//               How It Works
+//             </Typography>
+
+//             <Typography
+//               variant="body1"
+//               color="#F3F4F6"
+//               mb={5}
+//               textAlign={{ xs: "center", md: "left" }}
+//               maxWidth={420}
+//               mx={{ xs: "auto", md: 0 }}
+//             >
+//               Simplify the onboarding process by showing how easy it is to start
+//               using CodeArena.
+//             </Typography>
+
+//             {/* STEPS */}
+//             <Box sx={{ position: "relative" }}>
+//               {steps.map((step, index) => (
+//                 <Box key={step} sx={{ mb: 3 }}>
+//                   <StepCard
+//                     number={String(index + 1).padStart(2, "0")}
+//                     title={step}
+//                   />
+//                 </Box>
+//               ))}
+//             </Box>
+//           </Grid>
+//         </Grid>
+//       </Box>
+
+//       {/* DECORATIVE ARROWS */}
+//       <Box
+//         component="img"
+//         src={arrow1}
+//         alt="arrow"
+//         sx={{
+//           position: "absolute",
+//           right: 640,
+//           top: "85%",
+//           width: 90,
+//           display: { xs: "none", md: "block" },
+//           pointerEvents: "none",
+//         }}
+//       />
+
+//       <Box
+//         component="img"
+//         src={arrow2}
+//         alt="arrow"
+//         sx={{
+//           position: "absolute",
+//           right: -40,
+//           top: "60%",
+//           width: 90,
+//           display: { xs: "none", md: "block" },
+//           pointerEvents: "none",
+//         }}
+//       />
+
+//       <Box
+//         component="img"
+//         src={arrow2}
+//         alt="arrow"
+//         sx={{
+//           position: "absolute",
+//           right: 40,
+//           top: "70%",
+//           width: 90,
+//           display: { xs: "none", md: "block" },
+//           pointerEvents: "none",
+//         }}
+//       />
+//     </Box>
+//   );
+// }
 import { Box, Typography } from "@mui/material";
 import StepCard from "./StepsCard";
 import women from "../../assets/images/Image.png";
-import Grid from "@mui/material/Grid";
 
 import arrow1 from "../../assets/vectors/arrow1.png";
 import arrow2 from "../../assets/vectors/arrow2.png";
@@ -164,90 +304,85 @@ export default function WorkSection() {
         position: "relative",
         overflow: "hidden",
         minHeight: { xs: "auto", md: "860px" },
+        bgcolor: "primary.main",
       }}
     >
-      {/* BACKGROUND */}
+      {/* CONTENT */}
       <Box
         sx={{
-          position: "absolute",
-          inset: 0,
-          display: "flex",
-          zIndex: 0,
+          position: "relative",
+          zIndex: 1,
+          px: { xs: 2, md: 6 },
+          display: "grid",
+          gridTemplateColumns: { xs: "1fr", md: "1fr 1fr" },
+          gap: { xs: 4, md: 6 },
+          alignItems: "start",
         }}
       >
-        <Box sx={{ width: "100%", bgcolor: "primary.main" }} />
+        {/* LEFT IMAGE */}
+        <Box sx={{ textAlign: { xs: "center", md: "left" } }}>
+          <Box
+            component="img"
+            src={women}
+            alt="Student"
+            sx={{
+              width: "100%",
+              maxWidth: { xs: 320, sm: 420, md: 520 },
+              mx: { xs: "auto", md: 0 },
+            }}
+          />
+        </Box>
+
+        {/* RIGHT CONTENT */}
+        <Box sx={{ position: "relative" }}>
+          <Typography
+            variant="h3"
+            color="white"
+            mb={2}
+            textAlign={{ xs: "center", md: "left" }}
+            pt={{ xs: 0, md: 4 }}
+          >
+            How It Works
+          </Typography>
+
+          <Typography
+            variant="body1"
+            color="#F3F4F6"
+            mb={5}
+            textAlign={{ xs: "center", md: "left" }}
+            maxWidth={420}
+            mx={{ xs: "auto", md: 0 }}
+          >
+            Simplify the onboarding process by showing how easy it is to start
+            using CodeArena.
+          </Typography>
+
+          {/* STEPS */}
+          <Box>
+            {steps.map((step, index) => (
+              <Box key={step} sx={{ mb: 3 }}>
+                <StepCard
+                  number={String(index + 1).padStart(2, "0")}
+                  title={step}
+                />
+              </Box>
+            ))}
+          </Box>
+        </Box>
       </Box>
 
-      {/* CONTENT */}
-      <Box sx={{ position: "relative", zIndex: 1, px: { xs: 2, md: 6 } }}>
-        <Grid container spacing={{ xs: 4, md: 6 }} alignItems="flex-start">
-          {/* LEFT IMAGE */}
-          <Grid item xs={12} md={6}>
-            <Box textAlign={{ xs: "center", md: "left" }}>
-              <Box
-                component="img"
-                src={women}
-                alt="Student"
-                sx={{
-                  width: "100%",
-                  maxWidth: { xs: 320, sm: 420, md: 520 },
-                  display: "block",
-                  mx: { xs: "auto", md: 0 },
-                }}
-              />
-            </Box>
-          </Grid>
-
-          {/* RIGHT CONTENT */}
-          <Grid item xs={12} md={6} position="relative">
-            <Typography
-              variant="h3"
-              color="white"
-              mb={2}
-              textAlign={{ xs: "center", md: "left" }}
-              pt={{ xs: 0, md: 4 }}
-            >
-              How It Works
-            </Typography>
-
-            <Typography
-              variant="body1"
-              color="#F3F4F6"
-              mb={5}
-              textAlign={{ xs: "center", md: "left" }}
-              maxWidth={420}
-              mx={{ xs: "auto", md: 0 }}
-            >
-              Simplify the onboarding process by showing how easy it is to start
-              using CodeArena.
-            </Typography>
-
-            {/* STEPS */}
-            <Box sx={{ position: "relative" }}>
-              {steps.map((step, index) => (
-                <Box key={step} sx={{ mb: 3 }}>
-                  <StepCard
-                    number={String(index + 1).padStart(2, "0")}
-                    title={step}
-                  />
-                </Box>
-              ))}
-            </Box>
-          </Grid>
-        </Grid>
-      </Box>
-
-      {/* DECORATIVE ARROWS (HIDE ON MOBILE) */}
+      {/* DECORATIVE ARROWS */}
       <Box
         component="img"
         src={arrow1}
         alt="arrow"
         sx={{
           position: "absolute",
-          right: 640,
-          top: "100%",
+          right: 120,
+          top: "75%",
           width: 90,
           display: { xs: "none", md: "block" },
+          pointerEvents: "none",
         }}
       />
 
@@ -257,36 +392,11 @@ export default function WorkSection() {
         alt="arrow"
         sx={{
           position: "absolute",
-          right: -40,
-          top: "60%",
+          right: -30,
+          top: "55%",
           width: 90,
           display: { xs: "none", md: "block" },
-        }}
-      />
-
-      <Box
-        component="img"
-        src={arrow2}
-        alt="arrow"
-        sx={{
-          position: "absolute",
-          right: 40,
-          top: "60%",
-          width: 90,
-          display: { xs: "none", md: "block" },
-        }}
-      />
-
-      <Box
-        component="img"
-        src={arrow1}
-        alt="arrow"
-        sx={{
-          position: "absolute",
-          right: -40,
-          top: "60%",
-          width: 90,
-          display: { xs: "none", md: "block" },
+          pointerEvents: "none",
         }}
       />
     </Box>
